de0f9258c52acffcfb80ccfd9954557d2146c703
Alla Kuzminova <alchik@gmail.com>
2020-12-26 22:22:27 +0000

    the latest commit

--- lib/nutrition_facts/scraper.rb
+++ lib/nutrition_facts/scraper.rb
@@ -6,6 +6,7 @@ require_relative 'nutrition_fact'
 class Scraper
   #attr_accessor :page
  @@page = nil
+ ALPHABET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   def self.get_page
     if(@@page == nil)
       @@page = Nokogiri::HTML(open("https://nutritionfacts.org/topics"))
@@ -13,15 +14,24 @@ class Scraper
       @@page
   end
 
-  def self.scrape_topics_list
-    #puts self.get_page.css(".topics-trending")
-     self.get_page.css(".topics-trending").css("a").each do |topic|
-     puts topic["href"]
-     puts topic["title"]
-     NutritionTopic.new(topic["title"], topic["href"])
-     end
-     #puts self.get_page.css(".topics-trending")
+  def self.scrape_popular_topics
+    self.create_topic_from_a_element(self.get_page.css(".topics-trending").css("a"))
+  end
+
+  def self.scrape_topics_alphabetically
+    ALPHABET.each do |letter|
+      doc = self.get_page.css('div#column_'+"#{letter}")
+      self.create_topic_from_a_element(doc.css('ul.list-unstyled').css('a'))
+    end
+  end
+
+  def self.create_topic_from_a_element(a_element)
+    a_element.each do |item|
+      puts item["href"]
+      puts item["title"]
+      NutritionTopic.new(topic["title"], topic["href"])
+    end
   end
 end
 
-Scraper.scrape_topics_list
+Scraper.scrape_topics_by_alphabet
