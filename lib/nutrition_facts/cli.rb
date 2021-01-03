8d3a3d68ab82d3cd448c4d119699244eaa932245
Alla Kuzminova <alchik@gmail.com>
2021-01-03 02:04:14 +0000

    latest

--- lib/nutrition_facts/cli.rb
+++ lib/nutrition_facts/cli.rb
@@ -14,14 +14,19 @@ def valid_choice?(input)
 end
 
 def start
+  puts "******************************************************************************************************************************************************************************************"
   puts "Which Nutrition topic would you like to explore?"
   puts "Please type in 'popular' or a letter of the alphabet (E.g.: 'a') to see all popular topics or all topics starting with the letter you typed"
+  puts "******************************************************************************************************************************************************************************************"
   puts ""
   choice = gets.chomp.downcase
   while !valid_choice?(choice)
     puts "Your input is invalid, please try again"
     choice = gets.chomp.downcase
   end
+  puts ""
+  puts "Retrieving information..."
+  puts ""
   if choice == "popular"
     Scraper.get_popular_topics
     NutritionTopic.display_popular_topics
@@ -33,9 +38,10 @@ def start
   list_of_topic_videos = []
   while list_of_topic_videos == nil || list_of_topic_videos.count == 0
     puts ""
+    puts "******************************************************************************************************************************************************************************************"
     puts "Please type in a topic from above list for more information"
+    puts "******************************************************************************************************************************************************************************************"
     input = gets.strip.chomp
-    puts ""
     puts "Retrieving information..."
     if choice == "popular"
       list_of_topic_videos = NutritionTopic.lookup_topic_info(input)
@@ -43,19 +49,33 @@ def start
       letter = input[0].capitalize
       list_of_topic_videos = NutritionTopic.lookup_topic_info_by_letter(input, letter)
     end
+    if list_of_topic_videos == nil || list_of_topic_videos.count == 0
+      puts ""
+      puts "Invalid Input"
+    end
   end
 
-  puts ""
-  puts "Which of the following subtopics, would you like to get more information on:"
   puts ""
   list_of_topic_videos.each do |video|
     puts "      - "+video.video_name
   end
+
+  selected_video = nil
+  while selected_video == nil
+    puts ""
+    puts "******************************************************************************************************************************************************************************************"
+    puts "Which of the above subtopics, would you like to get more information on:"
+    puts "******************************************************************************************************************************************************************************************"
+    puts ""
+    input = gets.chomp
+    selected_video = list_of_topic_videos.find { |video| video.video_name == input}
+    if(selected_video == nil)
+      puts ""
+      puts "Invalid Input"
+    end
+  end
   puts ""
-  input = gets.chomp
-  selected_video = list_of_topic_videos.find { |video| video.video_name = input}
-  puts ""
-  puts "===================================================================================================================="
+  puts "=========================================================================================================================================================================================="
   puts "Video Name: "
   puts ""+selected_video.video_name
   puts ""
@@ -64,7 +84,7 @@ def start
   puts ""
   puts "Video Doctor's Note: "
   puts ""+selected_video.doctors_note
-  puts "===================================================================================================================="
+  puts "=========================================================================================================================================================================================="
 end
 
 def print_restaurant(restaurant)
