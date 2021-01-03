cc62f9cd75c9dc7b00ab54d06c2e2621c7b27ccf
Alla Kuzminova <alchik@gmail.com>
2021-01-03 02:13:01 +0000

    latest

--- lib/nutrition_facts/cli.rb
+++ lib/nutrition_facts/cli.rb
@@ -14,77 +14,86 @@ def valid_choice?(input)
 end
 
 def start
-  puts "******************************************************************************************************************************************************************************************"
-  puts "Which Nutrition topic would you like to explore?"
-  puts "Please type in 'popular' or a letter of the alphabet (E.g.: 'a') to see all popular topics or all topics starting with the letter you typed"
-  puts "******************************************************************************************************************************************************************************************"
-  puts ""
-  choice = gets.chomp.downcase
-  while !valid_choice?(choice)
-    puts "Your input is invalid, please try again"
+  continue = "yes"
+  while continue == "yes"
+    puts "******************************************************************************************************************************************************************************************"
+    puts "Which Nutrition topic would you like to explore?"
+    puts "Please type in 'popular' to see all popular topics or a letter of the alphabet (E.g.: 'a') to see all topics starting with the letter you typed"
+    puts "******************************************************************************************************************************************************************************************"
+    puts ""
     choice = gets.chomp.downcase
-  end
-  puts ""
-  puts "Retrieving information..."
-  puts ""
-  if choice == "popular"
-    Scraper.get_popular_topics
-    NutritionTopic.display_popular_topics
-  else
-      Scraper.get_all_topics
-      NutritionTopic.display_topics_by_letter(choice.capitalize)
-  end
-
-  list_of_topic_videos = []
-  while list_of_topic_videos == nil || list_of_topic_videos.count == 0
+    while !valid_choice?(choice)
+      puts "Your input is invalid, please try again"
+      choice = gets.chomp.downcase
+    end
     puts ""
-    puts "******************************************************************************************************************************************************************************************"
-    puts "Please type in a topic from above list for more information"
-    puts "******************************************************************************************************************************************************************************************"
-    input = gets.strip.chomp
     puts "Retrieving information..."
+    puts ""
     if choice == "popular"
-      list_of_topic_videos = NutritionTopic.lookup_topic_info(input)
+      Scraper.get_popular_topics
+      NutritionTopic.display_popular_topics
     else
-      letter = input[0].capitalize
-      list_of_topic_videos = NutritionTopic.lookup_topic_info_by_letter(input, letter)
+        Scraper.get_all_topics
+        NutritionTopic.display_topics_by_letter(choice.capitalize)
     end
-    if list_of_topic_videos == nil || list_of_topic_videos.count == 0
-      puts ""
-      puts "Invalid Input"
-    end
-  end
 
-  puts ""
-  list_of_topic_videos.each do |video|
-    puts "      - "+video.video_name
-  end
-
-  selected_video = nil
-  while selected_video == nil
-    puts ""
-    puts "******************************************************************************************************************************************************************************************"
-    puts "Which of the above subtopics, would you like to get more information on:"
-    puts "******************************************************************************************************************************************************************************************"
-    puts ""
-    input = gets.chomp
-    selected_video = list_of_topic_videos.find { |video| video.video_name == input}
-    if(selected_video == nil)
+    list_of_topic_videos = []
+    while list_of_topic_videos == nil || list_of_topic_videos.count == 0
       puts ""
-      puts "Invalid Input"
+      puts "******************************************************************************************************************************************************************************************"
+      puts "Please type in a topic from above list for more information"
+      puts "******************************************************************************************************************************************************************************************"
+      input = gets.strip.chomp
+      puts "Retrieving information..."
+      if choice == "popular"
+        list_of_topic_videos = NutritionTopic.lookup_topic_info(input)
+      else
+        letter = input[0].capitalize
+        list_of_topic_videos = NutritionTopic.lookup_topic_info_by_letter(input, letter)
+      end
+      if list_of_topic_videos == nil || list_of_topic_videos.count == 0
+        puts ""
+        puts "Invalid Input"
+      end
     end
+
+    puts ""
+    list_of_topic_videos.each do |video|
+      puts "      - "+video.video_name
+    end
+
+    selected_video = nil
+    while selected_video == nil
+      puts ""
+      puts "******************************************************************************************************************************************************************************************"
+      puts "Which of the above subtopics, would you like to get more information on:"
+      puts "******************************************************************************************************************************************************************************************"
+      puts ""
+      input = gets.chomp
+      selected_video = list_of_topic_videos.find { |video| video.video_name == input}
+      if(selected_video == nil)
+        puts ""
+        puts "Invalid Input"
+      end
+    end
+    puts ""
+    puts "=========================================================================================================================================================================================="
+    puts "Video Name: "
+    puts ""+selected_video.video_name
+    puts ""
+    puts "Video youtube url: "
+    puts ""+selected_video.video_url
+    puts ""
+    puts "Video Doctor's Note: "
+    puts ""+selected_video.doctors_note
+    puts "=========================================================================================================================================================================================="
+
+    puts "******************************************************************************************************************************************************************************************"
+    puts "Would like to continue exploring, please type in yes or no"
+    puts "******************************************************************************************************************************************************************************************"
+    continue = gets.chomp.downcase
+    puts ""
   end
-  puts ""
-  puts "=========================================================================================================================================================================================="
-  puts "Video Name: "
-  puts ""+selected_video.video_name
-  puts ""
-  puts "Video youtube url: "
-  puts ""+selected_video.video_url
-  puts ""
-  puts "Video Doctor's Note: "
-  puts ""+selected_video.doctors_note
-  puts "=========================================================================================================================================================================================="
 end
 
 def print_restaurant(restaurant)
